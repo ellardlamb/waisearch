a2d1f635f6d2c08c20677a5376c25e4092bc732c
ellardlamb <4511395+ellardlamb@users.noreply.github.com>
2026-09-21 23:01:46 -0500

    use js to open new tab on enter input

--- streamlit_waisearch.py
+++ streamlit_waisearch.py
@@ -1,13 +1,23 @@
 import streamlit as st
 import webbrowser
 import urllib.parse
+from streamlit.components.v1 import html
 
 def on_input():
     query = st.session_state["input"]
     encoded_query = urllib.parse.quote_plus(query)
     url = f"https://www.google.com/search?q={encoded_query}&udm=14"
 
-    webbrowser.open_new_tab(url)
+    # webbrowser.open_new_tab(url) # Only works when running locally, not in Streamlit Cloud
+    open_page(url)
+
+def open_page(url):
+    open_script= """
+        <script type="text/javascript">
+            window.open('%s', '_blank').focus();
+        </script>
+    """ % (url)
+    html(open_script)
 
 st.set_page_config(
     page_title="waiSearch",
